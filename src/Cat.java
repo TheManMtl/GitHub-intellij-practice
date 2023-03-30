5e4241004b3230e24b0dd84d1e41705ef329fa8b
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-03-29 23:55:18 -0400

    branch changes

--- src/Cat.java
+++ src/Cat.java
@@ -5,6 +5,25 @@
 
 public class Cat extends Felines{
 
+    private String name;
+    private String breed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
     @Override
     public void eat() {
         System.out.println("Cat is eating");
